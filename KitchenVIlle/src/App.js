a07e64ee884176c6b869e1ea90307d42b9e3c159
sidra tariq <sidratariq48@gmail.com>
2021-10-22 21:11:58 +0500

    BasicTemplate

--- KitchenVIlle/src/App.js
+++ KitchenVIlle/src/App.js
@@ -1,62 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import Recipe from './Recipie';
 import './App.css';
+import Nav from './Header/Nav';
+import About from './Dashboard/About';
+import Shop from './CommingSoon/Shop';
+import Login from './Authentication/Login'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  const APP_ID = "08706b89";
-  const APP_KEY = "30f4b2c1b4937486525d9a0817552ebe";
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
-
-  const updateSearch = e =>{
-    setSearch(e.target.value);
-    console.log(search)
-  }
-
-  const getSearch = e => {
-    e.preventDefault();
-    setQuery(search);
-  };
-
-  useEffect(() => {
-    document.title="KitchenVille"
-    const getRecipes = async () => {
-      const response = await fetch(
-        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      );
-      const data = await response.json();
-      setRecipes(data.hits);
-    };
-    getRecipes();
-  }, [query]);
 
   return (
-    <div className="App">
-      <header className="header">
-        <h1 className="title">Strange Recipes Finder</h1>
-        <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-        <button  className="search-button" type="submit">
-          Search 
-        </button>
-      </form>
-      
-      </header>
-      <div className="recipes">
-        {recipes.map((recipe, index) => (
-          <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-          />
-        ))}
+    <Router>
+      <div className="App">
+        <Nav />
+        <Login/>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 };
 
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
+
 export default App;
+
